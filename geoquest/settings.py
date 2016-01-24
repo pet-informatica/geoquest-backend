af3052f1a020d8d7b14da046c1a4715dc786bd33
Bruno Soares da Silva <brunosilvapaz@gmail.com>
2016-01-24 01:40:27 -0300

    Adding whitenoise to support static files

--- geoquest/settings.py
+++ geoquest/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
