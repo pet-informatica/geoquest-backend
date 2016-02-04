02c3cafab65eaa874569684ccb59357a9474be7d
Bruno Silva <bss3@cin.ufpe.br>
2016-02-04 00:24:13 -0300

    Facebook Login via token working integrated with the rest api

--- geoquest/settings.py
+++ geoquest/settings.py
@@ -39,11 +39,16 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'django.contrib.sites',
+	'rest_framework',
+	'rest_framework.authtoken',
+	'rest_auth',
 	'allauth',
 	'allauth.account',
+	'rest_auth.registration',
 	'allauth.socialaccount',
 	'allauth.socialaccount.providers.facebook',
 	'allauth.socialaccount.providers.twitter',
+
 ]
 
 MIDDLEWARE_CLASSES = [
--- geoquest/urls.py
+++ geoquest/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from geoquest.social_login.views import FacebookLogin
+
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^accounts/', include('allauth.urls')),
+	url(r'^rest-auth/', include('rest_auth.urls')),
+	url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+	url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
 ]
